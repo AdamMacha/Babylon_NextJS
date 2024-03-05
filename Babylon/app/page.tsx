f84f4683c024cc61bb98e0b155543b8a68cdba2d
AdamMacha <macha.adam@icloud.com>
2024-03-06 00:49:05 +0100

    final version v1.1.0

--- Babylon/app/page.tsx
+++ Babylon/app/page.tsx
@@ -1,12 +1,16 @@
 import Hero from "@/components/main/Hero";
+import Encryption from "@/components/main/Encryption";
 import Footer from "@/components/main/Footer";
 
 export default function Home() {
   return (
     <main className='h-full w-full'>
-      <div className="flex flex-col gap-20">
+      <div className="flex flex-col ">
         <Hero/>
+        <div className="flex flex-col gap-20">
+        <Encryption/>
         <Footer/>
+        </div>
       </div>
     </main>
   )
